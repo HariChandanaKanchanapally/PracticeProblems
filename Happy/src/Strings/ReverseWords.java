9bc023c183ef0c86fe4284baaa61979995d2e7bd
Chandana Kanchanapally <hkanchan@asu.edu>
2015-12-16 16:07:30 +0530

    ReverseWords Modif

--- Happy/src/Strings/ReverseWords.java
+++ Happy/src/Strings/ReverseWords.java
@@ -3,23 +3,27 @@ package Strings;
 import java.util.Scanner;
 
 public class ReverseWords {
-public static void main(String args[]) {
-	Scanner sc = new Scanner(System.in);
-	String str = sc.nextLine();
-	
-	String[] str2 = str.split(" ");
-	for (int i=0;i<str2.length;i++) {
-		char[] c = str2[i].toCharArray();
-		for(int j=0,k=str2[i].length()-1;j<str2[i].length()/2;j++,k--) {
+	public static String reverse(String str) {
+		char[] c = str.toCharArray();
+		for(int j=0,k=str.length()-1;j<str.length()/2;j++,k--) {
 			char temp;
 			temp = c[j];
 			c[j] = c[k];
 			c[k] = temp;
 			
 		}
-		System.out.print(new String(c)+" ");
+		return new String(c);
 	}
-
+public static void main(String args[]) {
+	Scanner sc = new Scanner(System.in);
+	String str = sc.nextLine();
+	
+	String[] str2 = str.split(" ");
+	String s = "";
+	for (int i=0;i<str2.length;i++) {
+		s += " "+reverse(str2[i]);
+	}
+	System.out.println(reverse(s));
 	sc.close();
 }
 }
